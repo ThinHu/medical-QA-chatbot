e820db8ff72408e43e04b498f136216f76bf0f71
ThienHuu <thienhuu1810@gmail.com>
2026-07-24 09:12:04 +0700

    feat: add greeting chat

--- frontend/app.py
+++ frontend/app.py
@@ -4,6 +4,18 @@ import httpx
 import os
 FASTAPI_URL = os.environ.get("FASTAPI_URL", "http://localhost:8080/api/chat")
 
+@cl.on_chat_start
+async def start_chat():
+    welcome_msg = (
+        "Xin chào, tôi là trợ lí ảo của phòng khám BKMed.\n"
+        "Tôi có thể hỗ trợ Anh/Chị các vấn đề sau:\n"
+        "1. **Điều trị & Xử lý:** Hướng dẫn sơ cứu, tư vấn dùng thuốc và hướng giải quyết triệu chứng.\n"
+        "2. **Nguyên nhân:** Giải đáp lý do gây bệnh hoặc tác dụng phụ của các loại thuốc.\n"
+        "3. **Mức độ nghiêm trọng:** Đánh giá độ nguy hiểm của triệu chứng và đưa ra cảnh báo khẩn cấp.\n"
+        "4. **Chẩn đoán:** Giải thích sơ bộ ý nghĩa của các triệu chứng và kết quả thủ thuật y khoa."
+    )
+    await cl.Message(content=welcome_msg).send()
+
 @cl.on_message
 async def on_message(message: cl.Message):
     session_id = cl.user_session.get("id")
